b5c89e5ee68bd0bb9deb424bbccddd982b51d3e3
Lars Kliesing <larskliesing@Larss-MacBook-Pro.local>
2019-02-28 19:58:32 +0100

    LGK logo & bug fix

--- neue-st-functions.php
+++ neue-st-functions.php
@@ -1,16 +1,18 @@
 <?php
 
-function neue_st_post_thumbnail() {
-	?>
-	<figure class="post-thumbnail">
-		<a class="post-thumbnail-inner" href="<?php the_permalink(); ?>" aria-hidden="true" tabindex="-1">
-			<?php if ( ! twentynineteen_can_show_post_thumbnail() ) : ?>
-				<img />
-			<?php else : ?>
-				<?php the_post_thumbnail( 'post-thumbnail' ); ?>
-			<?php endif; ?>
-		</a>
-	</figure>
+if (!function_exists("neue_st_post_thumbnail")) {
+	function neue_st_post_thumbnail() {
+		?>
+		<figure class="post-thumbnail">
+			<a class="post-thumbnail-inner" href="<?php the_permalink(); ?>" aria-hidden="true" tabindex="-1">
+				<?php if ( ! twentynineteen_can_show_post_thumbnail() ) : ?>
+					<img />
+				<?php else : ?>
+					<?php the_post_thumbnail( 'post-thumbnail' ); ?>
+				<?php endif; ?>
+			</a>
+		</figure>
 
-	<?php
+		<?php
+	}
 }
